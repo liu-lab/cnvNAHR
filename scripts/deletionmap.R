d5c7628ff5363548c59d2f8728d5d3a37e65bb1b
byuan007 <yuanbo1007@gmail.com>
2021-02-02 15:03:42 -0800

    Update deletionmap.R

--- scripts/deletionmap.R
+++ scripts/deletionmap.R
@@ -1,3 +1,11 @@
 # setwd() to working directory
 del.all <- read.table("NAHR_GRCh38.withgenes.txt", sep="\t", stringsAsFactors = F, header = T)
-del.curated <- subset(del.all, freqCMA != "NA", select = c(seqnames, start, end))
+del.all.nochr17alt <- subset(del.all, seqnames != "chr17_GL000258v2_alt")
+
+idx.17q23 <- del.all[which(del.all$seqnames=="chr17_GL000258v2_alt" & del.all$end == 1355937),]
+idx.17q23$start <- 45607798 
+idx.17q23$end <- 46210001 
+idx.17q23$seqnames <-"chr17"
+
+del.all.modified <- rbind(del.all.nochr17alt, idx.17q23)
+del.all.modified.curated <- subset(del.all.modified, freqCMA!="NA")
