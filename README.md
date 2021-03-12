ccf1ba69982acccb50b678ccf2bffe3c8b4e265b
Peng <pengfeil@smirnov>
2021-03-12 00:40:37 -0600

    medRxiv version 2

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cnvNAHR
+
+Different sections of the scripts should be run in the following order. 
+
+I. To preprocess the data source needed for the analyses:
+  1. ./scripts/genes_inheritance.R
+        This is used to define recessive and dominant gene IDs. 
+  2. ./scripts/prepare_gnomadSNV.R
+     ./scripts/prepare_gnomadSV.R
+        These are used to prepare high-quality loss-of-function variants from gnomAD. 
+  3. ./scripts/prepare_clinvar_AR.R
+        This is used to prepare high-confidence pathogenic or likely pathogenic variants for recessive traits from ClinVar. 
+      
+II. To generate a genome-wide predicted NAHR map and to annotate the map, the following scripts can be run:
+      ./scripts/NAHRmap.GRCh38.R
+      ./scripts/NAHRmap.GRCh37.R
+    Then, the following script is used to plot the genome-wide NAHR-deletion map:
+      ./scripts/deletionmap.R
+  
+III. The following is the main code that produces the major analytical results of the manuscript. 
+      ./scripts/carrierBurden.R
--- scripts/deletionmap.R
+++ scripts/deletionmap.R
@@ -1,4 +1,3 @@
-# setwd() to working directory
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 BiocManager::install(version = "3.12")
@@ -7,8 +6,9 @@ BiocManager::install("karyoploteR")
 library(karyoploteR)
 library(IRanges)
 library(GenomicRanges)
+library(rtracklayer)
 
-del.all <- read.table("NAHR_GRCh38.withgenes.txt", sep="\t", stringsAsFactors = F, header = T)
+del.all <- read.table("./output/NAHR_GRCh38.withgenes.tsv", sep="\t", stringsAsFactors = F, header = T)
 del.all.nochr17alt <- subset(del.all, seqnames != "chr17_GL000258v2_alt")
 
 idx.17q23 <- del.all[which(del.all$seqnames=="chr17_GL000258v2_alt" & del.all$end == 1355937),]
@@ -18,28 +18,19 @@ idx.17q23$seqnames <-"chr17"
 
 del.all.modified <- rbind(del.all.nochr17alt, idx.17q23)
 del.all.modified.curated <- subset(del.all.modified, freqCMA!="NA")
-del.all.modified.gr <- with(del.all.modified, GRanges(del.all.modified$seqnames, IRanges(start=as.numeric(del.all.modified$start), end=as.numeric(del.all.modified$end)))) #hg38
-del.all.modified.curated.gr <- with(del.all.modified.curated, GRanges(del.all.modified.curated$seqnames, IRanges(start=as.numeric(del.all.modified.curated$start), end=as.numeric(del.all.modified.curated$end)))) #hg38
+del.all.modified.gr <- with(del.all.modified, GRanges(seqnames, IRanges(start=as.numeric(start), end=as.numeric(end)))) #hg38
+del.all.modified.curated.gr <- with(del.all.modified.curated, GRanges(seqnames, IRanges(start=as.numeric(start), end=as.numeric(end)))) #hg38
+
+mySession = browserSession("UCSC")
+genome(mySession) <- "hg38"
+segdup.raw <- getTable(ucscTableQuery(mySession, track="genomicSuperDups"))
 
 SegDup.hg38 <- read.table("C:/Users/byuan/OneDrive - SCH/Work/Manuscript_ongoing/NAHR/segdup38.txt", sep = "\t", header=F, stringsAsFactors = FALSE)
-SegDup.hg38.gr <- with(SegDup.hg38, GRanges(SegDup.hg38$V2, IRanges(start=as.numeric(SegDup.hg38$V3), end=as.numeric(SegDup.hg38$V4)))) #hg38
+SegDup.hg38.gr <- with(segdup.raw, GRanges(chrom, IRanges(start=as.numeric(chromStart), end=as.numeric(chromEnd)))) #hg38
 
 #all in one plot
+pdf(file = "./output/NAHR_GRCh38.plot.pdf")
 kp <- plotKaryotype(plot.type=2)
-kpPlotDensity(kp, data=SegDup.hg38.gr)
-kpPlotRegions(kp, data=del.all.modified.gr, data.panel=2,col="#008000")
-
-kp <- plotKaryotype(plot.type=2)
-kpPlotDensity(kp, data=SegDup.hg38.gr)
-kpPlotRegions(kp, data=del.all.modified.curated.gr, data.panel=2,col="#008000")
-
-#split
-kp <- plotKaryotype(genome="hg38", plot.type=2, chromosomes = c("chr1","chr2","chr3","chr4","chr5", "chr6","chr7","chr8","chr9","chr10","chr11","chr12"))
-kp <- plotKaryotype(genome="hg38", plot.type=2, chromosomes = c("chr13","chr14","chr15","chr16","chr17", "chr18","chr19","chr20","chr21","chr22","chrX","chrY"))
 kpPlotDensity(kp, data=SegDup.hg38.gr, window.size = 1000)
-kpPlotRegions(kp, data=del.all.modified.gr, data.panel=2, col="#008000")
-
-kp <- plotKaryotype(genome="hg38", plot.type=2, chromosomes = c("chr1","chr2","chr3","chr4","chr5", "chr6","chr7","chr8","chr9","chr10","chr11","chr12"))
-kp <- plotKaryotype(genome="hg38", plot.type=2, chromosomes = c("chr13","chr14","chr15","chr16","chr17", "chr18","chr19","chr20","chr21","chr22","chrX","chrY"))
-kpPlotDensity(kp, data=SegDup.hg38.gr, window.size = 1000)
-kpPlotRegions(kp, data=del.all.modified.curated.gr, data.panel=2, col="#008000")
+kpPlotRegions(kp, data=del.all.modified.gr, data.panel=2,col="#008000", r0 = 0, r1=3.5)
+dev.off()
